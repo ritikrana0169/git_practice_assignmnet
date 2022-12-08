0c87bd6730d220389e5510acd3a0fe19c6f86e72
ritik rana <Ritik>
2022-12-08 19:43:14 +0530

    done

---
+++ prime.js
@@ -0,0 +1,14 @@
+function prime(n){
+    if(n<2 || n==undefined){
+        return false;
+    }else{
+        for(let i=2; i<n**0.5; i++){
+            if(n%i==0){
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+prime(5);
